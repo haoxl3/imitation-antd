d9877b3815d812d1ac63218b32fdc059ecf6b738
haoxiaoli <haoxiaoli@baidu.com>
2021-10-25 23:35:10 +0800

    button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Button, {ButtonType, ButtonSize} from './components/Button/button.tsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button disabled>hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
