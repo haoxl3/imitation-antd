9b58d813009ced16f373ccf33a7a19e7bc83f603
haoxiaoli <haoxiaoli@baidu.com>
2021-10-29 16:27:51 +0800

    menu传参

--- src/components/Menu/menuItem.tsx
+++ src/components/Menu/menuItem.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import classNames from 'classnames';
+import {MenuContext} from './menu'
 
 export interface MenuItemProps {
-    index?: number;
+    index: number;
     disabled?: boolean;
     className?: string;
     style?: React.CSSProperties;
@@ -10,11 +11,21 @@ export interface MenuItemProps {
 
 const MenuItem: React.FC<MenuItemProps> = (props) => {
     const {index, disabled, className, style, children} = props;
+    // 获取父组件传来的值
+    const context = useContext(MenuContext);
     const classes = classNames('menu-item', className, {
-        'is-disabled': disabled
+        'is-disabled': disabled,
+        'is-active': context.index === index
     });
+    // 调用父组件传来的方法
+    const handleClick = () => {
+        // 当有父组件传来的onSelect方法，且disabled不为true才可调用点击的回调函数
+        if (context.onSelect && !disabled) {
+            context.onSelect(index);
+        }
+    }
     return (
-        <li className={classes} style={style}>{children}</li>
+        <li className={classes} style={style} onClick={handleClick}>{children}</li>
     )
 }
 
