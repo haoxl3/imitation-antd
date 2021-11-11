3dff2e98325bbca152291a9fa8a050eca57a8e72
haoxiaoli <haoxiaoli@baidu.com>
2021-11-11 17:39:00 +0800

    上传前验证文件及重命名文件

---
+++ src/components/Upload/upload.tsx
@@ -0,0 +1,94 @@
+import React, {ChangeEvent, FC, useRef} from 'react';
+import axios from 'axios';
+import Button from '../Button/button';
+
+export interface UploadProps {
+    action: string;
+    beforeUpload?: (file: File) => boolean | Promise<File>;
+    onProgress?: (percentage: number, file: File) => void;
+    onSuccess?: (data: any, file: File) => void;
+    onError?: (err: any, file: File) => void;
+    onChange?: (file: File) => void;
+}
+
+export const Upload: FC = (props) => {
+    const {action, onProgress, beforeUpload, onSuccess, onError, onChange} = props;
+    const fileInput = useRef(null);
+    const handleClick = () => {
+        if (fileInput.current) {
+            fileInput.current.click();
+        }
+    }
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        if (!files) {
+            return;
+        }
+        uploadFiles(files);
+        if (fileInput.current) {
+            fileInput.current.value = '';
+        }
+    }
+    const uploadFiles = (files: FileList) => {
+        let postFiles = Array.from(files);
+        postFiles.forEach(file => {
+            if (!beforeUpload) {
+                post(file);
+            } else {
+                const result = beforeUpload(file);
+                if (result && result instanceof Promise) {
+                    result.then(processedFile => {
+                        post(processedFile);
+                    });
+                } else if (result !== false) {
+                    post(file);
+                }
+            }
+        });
+    }
+    const post = (file: File) => {
+        const formData = new FormData();
+        formData.append(file.name, file);
+        axios.post(action, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            onUploadProgress: e => {
+                let percentage = Math.round((e.loaded * 100) / e.total) || 0;
+                if (percentage < 100) {
+                    if (onProgress) {
+                        onProgress(percentage, file);
+                    }
+                }
+            }
+        }).then(resp => {
+            if (onSuccess) {
+                onSuccess(resp.data, file);
+            }
+            if (onChange) {
+                onChange(file);
+            }
+        }).catch(err => {
+            if (onError) {
+                onError(err, file);
+            }
+            if (onChange) {
+                onChange(file);
+            }
+        })
+    }
+    return (
+        <div className="viking-upload-component">
+            <Button btnType="primary" onClick={handleClick}>upload file</Button>
+            <input
+                className="viking-file-input"
+                style={{display: 'none'}}
+                ref={fileInput}
+                type="file"
+                onChange={handleFileChange}
+            />
+        </div>
+    );
+}
+
+export default Upload;
